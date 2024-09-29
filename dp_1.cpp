79ed4f2825c81cf6b3edf7afa59a872576c4315d
asus test <msa-99@outlook.com>
2024-09-29 14:51:22 +0600

    dp txt

---
+++ dp_1.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*
+ Dynamic-Programming: hocce ekti optimized recursive approach. Sadaron recursive solution e onek gulu extra recursive call hoye zay zar zonno complexity bere zay. Memoization kore sei extra recursive call guluke komanu zay, zeta ke optimization bola hoy. Memoization er maddhoyme kuno recursive call ke optimize korai hocce Dynamic Programming.
+ 
+*/
+int main(){
+
+    return 0;
+}
