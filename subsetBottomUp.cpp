a2fccf5ca01059d0a58a74dc845c6422046654bf
asus test <msa-99@outlook.com>
2024-10-05 02:06:59 +0600

    subsetsum

--- subsetBottomUp.cpp
+++ subsetBottomUp.cpp
@@ -14,5 +14,25 @@ int main(){
     for(int i=1; i<=s; i++){
         dp[0][i]=false;
     }
+    //i=1, j=0 hobe karon array value o hote pare.
+    for(int i=1; i<=n; i++){
+        for(int j=0; j<=s; j++){
+            if(a[i-1]<=j){
+                dp[i][j]=dp[i-1][j-a[i-1]]||dp[i-1][j];
+            }
+            else {
+                dp[i][j]=dp[i-1][j];
+            }
+        }
+    }
+    for(int i=0; i<=n; i++){
+        for(int j=0; j<=s; j++){
+            cout<<dp[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    
+    if(dp[n][s]) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
     return 0;
 }
